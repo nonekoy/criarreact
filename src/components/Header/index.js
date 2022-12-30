38a693e594fb8331b762f06b1a666ac24f3e6eba
Vinícius de Carvalho Pereira <95034279+nonekoy@users.noreply.github.com>
2022-12-30 08:05:33 -0300

    Add files via upload

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import './header.css'
+export default function Header(){
+    return(
+        <div className="Principal">
+            <ul className="Secundario">
+                <li>Início</li>
+                <li>Eventos</li>
+                <li>Contato</li>
+            </ul>
+        </div>
+    )
+}
